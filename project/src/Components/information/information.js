ec67e867b23f2b06add8cf860e1ae8faf2aad584
Denisse_ <denisse.alfaro@ucr.ac.cr>
2020-10-22 14:48:11 -0600

    feat: Delete item implemented. Pending refactor

--- project/src/Components/information/information.js
+++ project/src/Components/information/information.js
@@ -3,20 +3,30 @@ import "./information.css";
 import Description from "../../Components/description/description";
 import GetLocal from "../../Helper/getLocal";
 
+  
+const deleteItem = (coll, type, id) => {
+  console.log(coll, type, id);
+  let arr = [];
+  arr = coll.filter((item) => !item.id.includes(id));
+  localStorage.setItem(type, JSON.stringify(arr));
+};
+
 
 const Information = ({coll, title}) => {
+  let collection = GetLocal(coll);
+  console.log(collection);
   return (
     <div>
       <Description
         title={title}
         description={`These are the ${title} that you have saved`}
       />
-      {GetLocal(coll) ? (
+      {collection ? (
         <div className="content">
           <ul>
-            {GetLocal(coll).map((item) => (
+            {collection.map((item) => (
               <li className="descriptionLi">
-                {item.name} <span>Delete item</span><br/>
+                {item.name} <span onClick={() => deleteItem(collection, coll, item.id)}>Delete item</span><br/>
                 {item.description}
               </li>
             ))}
