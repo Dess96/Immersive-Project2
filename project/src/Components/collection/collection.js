ad2685f29170412ccd590618bd395ce7fa0064f3
Denisse_ <denisse.alfaro@ucr.ac.cr>
2020-10-23 10:05:59 -0600

    feat: Collection media queries added

--- project/src/Components/collection/collection.js
+++ project/src/Components/collection/collection.js
@@ -16,7 +16,7 @@ const Collection = () => {
   return (
     <div className="content">
       <div className="title">
-        <h2>My collections</h2>
+        <h2 className="descTitle">My collections</h2>
         <hr />
       </div>
       <div className="coll">
